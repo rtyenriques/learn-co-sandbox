177d4efb3c64deae998dd93c05a342057e40b139
Ross Enriques <rtyenriques@gmail.com>
2020-08-06 00:11:55 +0000

    Automatically backed up by Learn

---
+++ java.js
@@ -0,0 +1,5 @@
+console.log("hi")
+let smoke = 3
+if (smoke > 1);
+return smoke
+console.log("smoke")
---
+++ looping.rb
@@ -0,0 +1,9 @@
+counter = 0
+
+loop do
+  counter +=log1
+  puts"Never pass a school bus if it has flashing red lights"
+    if counter >=10
+  end    
+end
+
